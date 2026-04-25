e773cf9c721f916e752a6dabd6a328b5e3e180e2
hayk-m-h <h.m.mnatsakanyan@gmail.com>
2026-04-25 11:13:52 +0400

    OrderApi

--- src/Services/Ordering/Ordering.API/Endpoints/GetOrdersByCustomer.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/GetOrdersByCustomer.cs
@@ -1,6 +1,25 @@
+using Ordering.Application.Orders.Queries.GetOrdersByCustomer;
+
 namespace Ordering.API.Endpoints;
 
-public class GetOrdersByCustomer
+public record GetOrderByCustomerResponse(IEnumerable<OrderDto> Orders);
+
+public class GetOrdersByCustomer : ICarterModule
 {
-    
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/orders/customer/{customerId}", async (Guid customerId, ISender sender) =>
+        {
+            var query = new GetOrdersByCustomerQuery(customerId);
+            var result = await sender.Send(query);
+            var response = result.Adapt<GetOrderByCustomerResponse>();
+            return Results.Ok(response);
+        })
+        .WithName("GetOrdersByCustomer")
+        .Produces<GetOrderByCustomerResponse>()
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .WithSummary("Get Orders By Customer")
+        .WithDescription("Gets orders for the specified customer id.");
+    }
 }
